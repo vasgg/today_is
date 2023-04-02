5ae6189a44c2ff97e68d492b5159f7cd2e95e7e8
vas g <vas.g@me.com>
2023-04-03 02:25:50 +0400

    records operations

--- bot/handlers/records.py
+++ bot/handlers/records.py
@@ -1,26 +1,133 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Command
-from bot.models import Record
-from bot.config import dp
+from aiogram.dispatcher import FSMContext
+import dateutil.parser
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+
+from bot.models import Record, User
+from bot.config import dp, logger
 from bot.replies import answer
-from bot.utils.keyboards import add_record_button
+from bot.utils.keyboards import add_record_keyboard, add_and_delete_keyboard
 from bot.database import session
 from sqlalchemy import literal
+from bot.utils.states import States
+from datetime import datetime, timedelta
+
+
+def records_counter(user_id: int) -> int:
+    query = session.query(Record).filter(Record.user_id == user_id)
+    count = query.count()
+    return count
+
+
+def all_records_reply(user_id: int) -> str:
+    all_records_query = session.query(Record).filter(Record.user_id == user_id)
+    all_records = session.execute(all_records_query)
+    user = session.query(User).filter(User.user_id == user_id).scalar()
+    offset = user.utc_offset
+    if not offset:
+        now = datetime.utcnow()
+    else:
+        now = datetime.utcnow() + timedelta(hours=offset)
+
+    event_number = 0
+    all_records_reply = ''
+    for row in all_records.fetchall():
+        event = row[0]
+        if now.day == event.event_date.day and now.month == event.event_date.month and now.year == event.event_date.year:
+            ans = ' <b>today</>'
+        elif now > event.event_date:
+            period = now - event.event_date
+            ans = (f' <b>{period.days}</> days ago')
+        else:
+            period = event.event_date - now
+            ans = f' in <b>{period.days + 1}</> days'
+
+        days_counter = now - event.event_date
+        event_number += 1
+        row = f"{event_number}. {event.event_name}{ans}\n"
+        all_records_reply += row
+    return all_records_reply
 
 
 @dp.message_handler(Command('records'))
 async def records_command(message: types.Message):
-    user_id = message.from_user.id
-    db_record = session.query(Record.user_id).filter(Record.user_id == user_id)
+    db_record = session.query(Record.user_id).filter(Record.user_id == message.from_user.id)
     result = session.query(literal(True)).filter(db_record.exists()).scalar()
     if not result:
-        await dp.bot.send_message(chat_id=user_id,
-                                  text=answer["records_reply_unregistered"], reply_markup=add_record_button)
+        await dp.bot.send_message(chat_id=message.from_user.id,
+                                  text=answer["records_reply_unregistered"], reply_markup=add_record_keyboard)
+    else:
+        await dp.bot.send_message(chat_id=message.from_user.id,
+                                  text=all_records_reply(message.from_user.id), reply_markup=add_and_delete_keyboard)
 
 
 @dp.callback_query_handler(text='add_record')
-async def vote_up_cb_handler(query: types.CallbackQuery):
-    user_id = query.from_user.id
-    await query.answer(cache_time=60)
-    await dp.bot.send_message(chat_id=user_id,
-                              text='testing stuff')
+async def event_name(call: types.CallbackQuery):
+    await dp.bot.send_message(chat_id=call.from_user.id,
+                              text=answer["event_name_reply"])
+    await States.Event_name.set()
+
+
+@dp.message_handler(state=States.Event_name)
+async def name_input(message: types.Message, state: FSMContext):
+    event_name = message.text
+    await state.update_data(event_name=event_name)
+    await dp.bot.send_message(chat_id=message.from_user.id,
+                              text=answer["event_date_reply"])
+    await States.Event_date.set()
+
+
+@dp.message_handler(state=States.Event_date)
+async def date_input(message: types.Message, state: FSMContext):
+    reply = message.text
+    try:
+        event_date = dateutil.parser.parse(reply, dayfirst=True)
+        mydata = await state.get_data()
+        event_name = mydata.get('event_name')
+        record = Record(user_id=message.from_user.id,
+                        event_name=event_name,
+                        event_date=event_date,
+                        created_at=datetime.utcnow())
+        session.add(record)
+        session.commit()
+        session.close()
+        await dp.bot.send_message(chat_id=message.from_user.id,
+                                  text=all_records_reply(message.from_user.id), reply_markup=add_and_delete_keyboard)
+        await state.reset_state(with_data=False)
+    except ValueError as e:
+        await message.answer(answer['value_error_reply'])
+        logger.debug(answer['value_error_log'], message.from_user.id, e, message.text)
+
+
+@dp.callback_query_handler(text='delete_record')
+async def delete_record(call: types.CallbackQuery):
+    query = session.query(Record).filter(Record.user_id == call.from_user.id)
+    count = query.count()
+    kb = ReplyKeyboardMarkup(row_width=4, one_time_keyboard=True, input_field_placeholder="", resize_keyboard=True)
+    [kb.insert(KeyboardButton(text=f'{record}')) for record in range(1, count + 1)]
+    [kb.insert(KeyboardButton(text='cancel', callback_data='cancel'))]
+    await dp.bot.send_message(chat_id=call.from_user.id,
+                              text=answer["event_delete_reply"], reply_markup=kb)
+    await States.Delete_record.set()
+
+
+@dp.message_handler(text='cancel', state=States.Delete_record)
+async def delete_record(call: types.CallbackQuery, state: FSMContext):
+    await state.reset_state(with_data=False)
+    await dp.bot.send_message(chat_id=call.from_user.id,
+                              text=all_records_reply(call.from_user.id))
+
+
+@dp.message_handler(state=States.Delete_record)
+async def date_input(message: types.Message, state: FSMContext):
+    record_number = int(message.text)
+    db_record = session.query(Record.user_id).filter(Record.user_id == message.from_user.id)
+    user_records = session.query(Record).filter(Record.user_id == message.from_user.id).all()
+    selected_record = user_records[user_records - 1]
+    session.delete(selected_record)
+    session.commit()
+    session.close()
+    await state.reset_state(with_data=False)
+    await dp.bot.send_message(chat_id=message.from_user.id,
+                              text=all_records_reply(message.from_user.id), reply_markup=add_and_delete_keyboard)
