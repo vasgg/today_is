1fbff038ec1f25598abf05c0fc705a072592254d
vas g <vas.g@me.com>
2023-04-06 00:48:54 +0400

    work on bot replies

--- bot/replies.py
+++ bot/replies.py
@@ -1,23 +1,27 @@
 answer = {
-    "settings": "For correct work between timezones please send your location, or push the button bellow",
-    "tools_reply": "Commands from this section allows you count days between dates.",
-    "days_counter_reply": "Enter your event date in format dd.mm.yyyy:",
-    "date_calculator_reply": "Enter the first date in format dd.mm.yyyy:",
-    "date_calculator_reply": "Enter the first date in format dd.mm.yyyy:",
-
+    "tools_reply": "Commands from this section allows you count days between dates. More functions to come...",
+    "days_counter_reply": "This function counts days between provided date and today.\n"
+                          "Enter your event date:\n\n"
+                          "<i>preferred format: dd.mm.yyyy</i>",
+    "date_calculator_first_reply": "This function counts days between provided two dates.\n"
+                                   "Enter the first date:\n\n"
+                                   "<i>preferred format: dd.mm.yyyy</i>",
+    "date_calculator_second_reply": "Enter the second date:\n\n"
+                                    "<i>preferred format: dd.mm.yyyy</i>",
     "start_reply": "Greetings, {}.\n"
-                   "You are user #{}.\n"
+                   "You are user <b>#{}</b>\n"
                    "Registration date: {}, {} days ago\n"
                    "\n"
-                   "Let's begin with <b>/today_is</> command\n"
-                   "\n"
-                   "For other options go to the <b>/help</> section.\n",
+                   "Let's begin with <b>/today_is</> command.\n\n"
+                   "For more info check the <b>/help</> section.\n",
     "records_reply_unregistered": "For adding your first event push the button bellow",
-    "records_reply_registered": "",
-    "today_is_reply": "<b>Today is:</>\n\n"
-                      "{}, {} ({} progress: {}%)\n"
-                      "Day {}/{} ({} progress: {}%)\n"
-                      "Week {}/{}\n\n"
+    "today_is_reply": "Today is:\n\n"
+                      "{}, {}, {}\n"
+                      "Day {}/{}\n"
+                      "Week {}/{}\n"
+                      "Month progress: {}%\n"
+                      "{} progress: {}%\n"
+                      "\n"
                       "For managing your events use command <b>/records</>",
     "location_reply": "Geo data recieved. Here's some info:\n\n"
                       "Latitude: {}\nLongitude: {}\nSunrise: {}\nSunset: {}\n"
@@ -25,8 +29,24 @@ answer = {
     "value_error_log": "User [{}] get error [{}] with data [{}]",
     "value_error_reply": "Please enter correct value. For example: "
                          "20.04.2000 or 20 Apr 2000",
-    "event_name_reply": "Enter name of your record: ",
-    "event_date_reply": "Enter date of your record: ",
-    "event_delete_reply": "For delete record press the button bellow",
-
+    "event_name_reply": "Enter name of your event: ",
+    "event_date_reply": "Enter date of your event: ",
+    "event_delete_reply": "To delete the record, press the button bellow",
+    "settings_reply": "In this section you can change you timezone settings by providing your "
+                      "current location. Also you get additional date and time information. "
+                      "You can push the button bellow, or send location to bot whenever you want.",
+    "help_reply": "By default this bot uses UTC timezone (London time),\n"
+                  "but you can update the timezone by sending your current location "
+                  "(working only from smartphone) or click the button in <b>/settings</> menu.\n"
+                  "\n"
+                  "Additional date functions under development,\n"
+                  "however, they are available in the <b>/tools</> section.\n"
+                  "\n"
+                  "<i>With any questions, suggestions or feedback\n"
+                  "please contact <b>@vas1stdas</b>.</>\n",
+    "statistics_reply": "Here's some statistics:\n\n"
+                        "Users in database: {}\n"
+                        "Records in database: {}\n"
+                        "Total commands processed: {}\n"
+                        "Commands processed from you: {}\n",
 }
