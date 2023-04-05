62bf65a101c9415b91f07de4bba04ca425c7d525
vas g <vas.g@me.com>
2023-04-06 00:53:56 +0400

    adding commands counter

--- bot/middleware.py
+++ bot/middleware.py
@@ -4,15 +4,19 @@ from aiogram.types import Message
 from bot.models import User
 from bot.database import session
 from sqlalchemy import select, literal
+from bot.config import redis
 
 
 class AuthMiddleware(BaseMiddleware):
     async def on_process_message(self, message: Message, data: dict):
-        # user_values = session.query(User).filter_by(id=message.from_user.id).first().__dict__
         now = datetime.utcnow()
-        # print(user_values)
+
         user = session.query(User.user_id).filter(User.user_id == message.from_user.id)
         user_exist = session.query(literal(True)).filter(user.exists()).scalar()
+
+        redis.incrby('total', 1)
+        redis.incrby(f'user:{message.from_user.id}', 1)
+
         if not user_exist:
             user = User(user_id=message.from_user.id,
                         firstname=message.from_user.first_name,
