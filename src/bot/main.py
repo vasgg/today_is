f801298b6a2af5f0a1c39a872561b8d4eda7a876
vasgg <vas.g@icloud.com>
2025-07-18 16:49:04 +0400

    working on main module

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,14 +1,14 @@
-import asyncio
+from asyncio import run
 import logging.config
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.fsm.storage.redis import RedisStorage
-from redis.asyncio import Redis
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from bot.config import Settings, get_settings
 from bot.handlers.base import router as base_router
+from bot.handlers.records import router as records_router
 from bot.handlers.errors import router as errors_router
 from bot.internal.commands import set_bot_commands
 from bot.internal.helpers import setup_logs
@@ -20,18 +20,11 @@ from database.db_connector import get_db
 
 
 async def main():
-    setup_logs("bot")
+    setup_logs("today_is_bot")
     settings: Settings = get_settings()
 
     bot = Bot(token=settings.bot.token.get_secret_value(), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    redis_client = Redis(
-        host=settings.redis.host,
-        port=settings.redis.port,
-        username=settings.redis.username,
-        password=settings.redis.password.get_secret_value(),
-        decode_responses=True,
-    )
-    storage = RedisStorage(redis_client)
+    storage = MemoryStorage()
     dispatcher = Dispatcher(storage=storage, settings=settings)
     db = get_db(settings)
 
@@ -44,14 +37,14 @@ async def main():
     dispatcher.startup.register(on_startup)
     dispatcher.shutdown.register(on_shutdown)
     dispatcher.startup.register(set_bot_commands)
-    dispatcher.include_routers(base_router, errors_router)
+    dispatcher.include_routers(base_router, errors_router, records_router)
 
     logging.info("bot started")
     await dispatcher.start_polling(bot)
 
 
 def run_main():
-    asyncio.run(main())
+    run(main())
 
 
 if __name__ == "__main__":
