560015ca51b7ec8e3c5d51dc67199d054fc7f4d5
vasgg <vas.g@icloud.com>
2025-07-18 16:47:32 +0400

    working on commands

--- src/bot/internal/commands.py
+++ src/bot/internal/commands.py
@@ -1,7 +1,11 @@
 from aiogram import Bot, types
 
 default_commands = [
-    types.BotCommand(command="/start", description="initial command"),
+    types.BotCommand(command="/start", description="first thing first"),
+    types.BotCommand(command="/today_is", description="main command"),
+    types.BotCommand(command="/records", description="all about your events"),
+    types.BotCommand(command="/settings", description="location and timezone stuff"),
+    types.BotCommand(command="/info", description="more info"),
 ]
 
 
