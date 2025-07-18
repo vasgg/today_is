880530a1fee009a763cd0631c905a46f39be18d3
vasgg <vas.g@icloud.com>
2025-07-18 14:52:10 +0400

    working on cruds

--- src/database/crud/user.py
+++ src/database/crud/user.py
@@ -1,5 +1,5 @@
 from aiogram.types import User as AiogramUser
-from sqlalchemy import Result, select
+from sqlalchemy import Result, func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.models import User
@@ -7,9 +7,11 @@ from database.models import User
 
 async def add_user_to_db(user: AiogramUser, db_session) -> User:
     new_user = User(
-        id=user.id,
-        fullname=user.full_name,
+        user_id=user.id,
+        firstname=user.first_name,
+        lastname=user.last_name,
         username=user.username,
+        language_code=user.language_code,
     )
     db_session.add(new_user)
     await db_session.flush()
@@ -17,7 +19,13 @@ async def add_user_to_db(user: AiogramUser, db_session) -> User:
 
 
 async def get_user_from_db_by_tg_id(telegram_id: int, db_session: AsyncSession) -> User | None:
-    query = select(User).filter(User.id == telegram_id)
+    query = select(User).filter(User.user_id == telegram_id)
     result: Result = await db_session.execute(query)
     user = result.scalar()
     return user
+
+
+async def get_users_count(db_session: AsyncSession) -> int:
+    query = select(func.count()).select_from(User)
+    result: Result = await db_session.execute(query)
+    return result.scalar_one()
